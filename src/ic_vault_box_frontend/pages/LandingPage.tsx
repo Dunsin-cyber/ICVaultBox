af4571d7137830aaa3018474b8594182df408ac8
Abisuwa <ayodejiabisuwa23@gmail.com>
2024-03-12 15:16:40 +0100

    project skeleton

---
+++ src/ic_vault_box_frontend/pages/LandingPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+function LandingPage() {
+  return <Box>Landing Page</Box>;
+}
+
+export default LandingPage;
---
+++ src/ic_vault_box_frontend/src/app.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "../pages/LandingPage";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </React.Fragment>
+  );
+};
+
+export default App;
