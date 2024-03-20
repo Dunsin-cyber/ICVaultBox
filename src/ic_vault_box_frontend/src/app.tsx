b9f2c7f146939b213c9ca8190e1af686e26c75da
Abisuwa <ayodejiabisuwa23@gmail.com>
2024-03-20 02:02:23 +0100

    added splash screen

--- src/ic_vault_box_frontend/src/app.tsx
+++ src/ic_vault_box_frontend/src/app.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import AddPasswordView from "../pages/AddPassword";
 import EditPasswordView from "../pages/EditPasswordView";
+import SplashScreen from "../pages/SplashScreen";
 
 const App = () => {
   const addNew = () => {};
@@ -10,8 +11,9 @@ const App = () => {
     <React.Fragment>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/new" element={<AddPasswordView addNew={addNew} />} />
-        <Route path="/edit" element={<EditPasswordView edit={addNew} />} />
+        <Route path="splashScreen" element={<SplashScreen />} />
+        <Route path="new" element={<AddPasswordView addNew={addNew} />} />
+        <Route path="edit" element={<EditPasswordView edit={addNew} />} />
       </Routes>
     </React.Fragment>
   );
