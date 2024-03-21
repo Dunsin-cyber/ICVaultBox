7c25e719160e9ef3a386bde3ed0231ac58e691f8
Abisuwa <ayodejiabisuwa23@gmail.com>
2024-03-21 05:43:14 +0100

    added context

---
+++ src/ic_vault_box_frontend/context/index.tsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import { Actor, Identity, ActorSubclass } from "@dfinity/agent";
+import { AuthClient } from "@dfinity/auth-client";
+import {
+  createActor,
+  canisterId,
+} from "../../declarations/ic_vault_box_backend";
+
+import { useLocation, useNavigate } from "react-router-dom";
+import { _SERVICE } from "../../declarations/ic_vault_box_backend/ic_vault_box_backend.did";
+
+export const AuthContext = React.createContext<{
+  Auth: any;
+  actor: ActorSubclass<_SERVICE> | undefined;
+  setActor: any;
+  iiAuth: boolean;
+  setIIAuth: any;
+  handleAuthenticated: any;
+  changeAuthStatus: any;
+  loggedIn: boolean;
+  setLoggedIn: any;
+}>({
+  Auth: undefined,
+  actor: undefined,
+  vdbActor: undefined,
+  iiAuth: false,
+  setIIAuth: undefined,
+  handleAuthenticated: undefined,
+  changeAuthStatus: undefined,
+  loggedIn: undefined,
+  setLoggedIn: undefined,
+});
+
+export const AuthProvider = ({ children }) => {
+  const [actor, setActor] = useState<ActorSubclass<_SERVICE>>();
+  const [iiAuth, setIIAuth] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  async function Auth(e) {
+    e.preventDefault();
+    console.log("You clicked me.");
+    // trackEvent({ category: "Authentication", action: "sign-in/sign-up" });
+    const authClient = await AuthClient.create();
+    if (await authClient.isAuthenticated()) {
+      handleAuthenticated(authClient);
+      // setTour(tour_);
+      if (location.pathname === "/") {
+        navigate("/");
+      }
+      setIIAuth(true);
+    }
+
+    const loginButton = document.getElementById("login") as HTMLButtonElement;
+
+    const days = BigInt(1);
+    const hours = BigInt(24);
+    const nanoseconds = BigInt(3600000000000);
+
+    const APPLICATION_NAME = "Nexai";
+    const APPLICATION_LOGO_URL =
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJR6SAoiTMNdmt6tabURYYwvSp9XcA9IgMjw&usqp=CAU";
+
+    const AUTH_PATH =
+      "/authenticate/?applicationName=" +
+      APPLICATION_NAME +
+      "&applicationLogo=" +
+      APPLICATION_LOGO_URL +
+      "#authorize";
+
+    await authClient.login({
+      onSuccess: async () => {
+        handleAuthenticated(authClient);
+        navigate("/");
+        setIIAuth(true);
+      },
+      windowOpenerFeatures:
+        `left=${window.screen.width / 2 - 525 / 2}, ` +
+        `top=${window.screen.height / 2 - 705 / 2},` +
+        `toolbar=0,location=0,menubar=0,width=525,height=705`,
+      identityProvider:
+        process.env.DFX_NETWORK === "ic"
+          ? "https://nfid.one" + AUTH_PATH
+          : // : process.env.LOCAL_II_CANISTER,
+            `http://rdmx6-jaaaa-aaaaa-aaadq-cai.localhost:4943`,
+      // Maximum authorization expiration is 8 days
+      maxTimeToLive: days * hours * nanoseconds,
+    });
+  }
+
+  async function handleAuthenticated(authClient: AuthClient) {
+    const identity = (await authClient.getIdentity()) as unknown as Identity;
+
+    const whoami_actor = createActor(canisterId as string, {
+      agentOptions: {
+        identity,
+      },
+    });
+
+    setActor(whoami_actor);
+
+    // Invalidate identity then render login when user goes idle
+    authClient.idleManager?.registerCallback(() => {
+      Actor.agentOf(whoami_actor)?.invalidateIdentity?.();
+    });
+  }
+
+  const changeAuthStatus = () => {
+    setIIAuth((prevState) => prevState !== prevState);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        Auth,
+        actor,
+        setActor,
+        iiAuth,
+        setIIAuth,
+        handleAuthenticated,
+        changeAuthStatus,
+        loggedIn,
+        setLoggedIn,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- src/ic_vault_box_frontend/src/app.tsx
+++ src/ic_vault_box_frontend/src/app.tsx
@@ -1,22 +1,61 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import AddPasswordView from "../pages/AddPassword";
 import EditPasswordView from "../pages/EditPasswordView";
 import SplashScreen from "../pages/SplashScreen";
+import { AuthClient } from "@dfinity/auth-client";
+import { AuthContext } from "../context";
 
 const App = () => {
+  const [actorRestated, setActorRestated] = React.useState<boolean>(false);
+  const navigate = useNavigate();
+
+  const { handleAuthenticated, setIIAuth, actor, vdbActor } =
+    React.useContext(AuthContext);
+
+  React.useEffect(() => {
+    const runOnMounth = async () => {
+      const authClient = await AuthClient.create();
+      if (await authClient.isAuthenticated()) {
+        console.log("IS AUTHENTICATED");
+        console.log(location.hash);
+        await handleAuthenticated(authClient);
+        setIIAuth(true);
+        setActorRestated(true);
+        navigate("/dashboard");
+        return;
+      } else {
+        console.log("NOT AUTHENTICATED");
+        navigate("/");
+        return;
+      }
+    };
+
+    runOnMounth();
+  }, []);
+
   const addNew = () => {};
-  return (
-    <React.Fragment>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="splashScreen" element={<SplashScreen />} />
-        <Route path="new" element={<AddPasswordView addNew={addNew} />} />
-        <Route path="edit" element={<EditPasswordView edit={addNew} />} />
-      </Routes>
-    </React.Fragment>
-  );
+
+  if (actorRestated) {
+    return (
+      <React.Fragment>
+        <Routes>
+          <Route path="/dashboard" element={<LandingPage />} />
+          <Route path="new" element={<AddPasswordView addNew={addNew} />} />
+          <Route path="edit" element={<EditPasswordView edit={addNew} />} />
+        </Routes>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <Routes>
+          <Route path="/" element={<SplashScreen />} />
+        </Routes>
+      </React.Fragment>
+    );
+  }
 };
 
 export default App;
